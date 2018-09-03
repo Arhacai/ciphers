f64ebbc37fe39dd646bbacb79666826071a8a3d9
Arhacai <arhacai@gmail.com>
2018-09-03 21:19:44 +0200

    Project refactorized

--- ciphers.py
+++ ciphers.py
@@ -3,54 +3,70 @@ import string
 
 
 class Cipher:
-    """Main class from which inherit all ciphers. It has two abstracs methods
+    """
+    Main class from which inherit all ciphers. It has two abstracs methods
     which will implemented by each subclass and three classmethods.
     One that includes a new level of encryption using a one time pad.
     Two other methods that split or join the text to show it in blocks of five.
     """
-    def encrypt(self):
+
+    LETTERS = string.ascii_uppercase
+
+    def __init__(self, pad='', in_blocks=False):
+        self.pad = pad
+        self.in_blocks = in_blocks
+
+    def encrypt(self, text):
         """Encryption method to be implemented on each cipher."""
         raise NotImplementedError()
 
-    def decrypt(self):
+    def decrypt(self, text):
         """Decryption method to be implemented on each cipher."""
         raise NotImplementedError()
 
-    @classmethod
-    def pad_encryption(cls, pad, text, encryption=True):
-        """Takes three arguments, one by default, to add another level of
+    def pad_encryption(self, text, encrypt=True):
+        """
+        Takes three arguments, one by default, to add another level of
         encryption to one of the ciphers used:
         - Pad is a numeric integer key that will be used to encrypt
-        - Text is the message that is going to be encrypted or decrypted
-        - Encryption takes True by default, so this method uses the pad to
+        - Message is the text that is going to be encrypted or decrypted
+        - Encryption is set to True by default, so this method uses the pad to
         encrypt the text. If set to False it reverse the encryption to get
         the text decrypted.
         """
-        num = len(text) // len(pad)
-        rest = len(text) % len(pad)
-        pad = pad * num + pad[:rest]
 
-        DICT = string.ascii_uppercase * 2
+        if self.pad == '':
+            return text
+
+        num = len(text) // len(self.pad)
+        rest = len(text) % len(self.pad)
+        pad = self.pad * num + self.pad[:rest]
+
+        letters = self.LETTERS * 2
 
         output = []
         for pos, char in enumerate(text):
             try:
-                value_char = DICT.index(char)
+                letter = letters.index(char)
             except ValueError:
                 output.append(char)
             else:
-                if encryption:
-                    output.append(DICT[value_char+int(pad[pos])])
+                if encrypt:
+                    output.append(letters[letter + int(pad[pos])])
                 else:
-                    output.append(DICT[value_char-int(pad[pos])])
+                    output.append(letters[letter - int(pad[pos])])
         return ''.join(output)
 
-    @classmethod
-    def split_in_blocks(cls, text):
-        """Takes one argument, text, and split it in blocks of five, using
+    def split_in_blocks(self, text):
+        """
+        Takes one argument, text, and split it in blocks of five, using
         some random special symbols to replace both spaces and the last block
         of characters if it doesn't get to be five.
         """
+
+        if not self.in_blocks:
+            return text
+
         symbols = ['@', '#', '$', '%', '&']
 
         # This num represent the characters needed to fill the last group of 5
@@ -62,24 +78,31 @@ class Cipher:
                 output.append(random.choice(symbols))
             else:
                 output.append(letter)
-        for num in range(add):
-            output.append(random.choice(symbols))
-
+        if add != 5:
+            for num in range(add):
+                output.append(random.choice(symbols))
         text = ''.join(output)
 
         output = []
         for index, letter in enumerate(text):
-            if (index+1) % 5 == 0:
-                output.append(letter + ' ')
+            if (index + 1) % 5 == 0:
+                if (index + 1) == len(text):
+                    output.append(letter)
+                else:
+                    output.append(letter + ' ')
             else:
                 output.append(letter)
         return ''.join(output)
 
-    @classmethod
-    def join_from_blocks(cls, text):
-        """Takes one argument, a text, and join it using some random symbols to
+    def join_from_blocks(self, text):
+        """
+        Takes one argument, a text, and join it using some random symbols to
         represent the spaces between words in the original message.
         """
+
+        if not self.in_blocks:
+            return text
+
         symbols = ['@', '#', '$', '%', '&']
 
         text = ''.join(text.split())
@@ -89,4 +112,280 @@ class Cipher:
                 output.append(' ')
             else:
                 output.append(letter)
+        while output[-1] == ' ':
+            del output[-1]
+
         return ''.join(output)
+
+    def encode(self, message):
+        return self.split_in_blocks(
+            self.pad_encryption(
+                self.encrypt(message)
+            )
+        )
+
+    def decode(self, message):
+        return self.decrypt(
+            self.pad_encryption(
+                self.join_from_blocks(message), False
+            )
+        )
+
+
+class Affine(Cipher):
+    """
+    Cipher that uses the Affine encryption method to codify a message.
+    This method will require two inputs from user:
+    - A first numeric key from a list of valid numbers.
+    - A second numeric key as any positive non zero integers.
+    """
+
+    ALPHA = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+    ALPHA_INV = [1, 9, 21, 15, 3, 19, 7, 23, 11, 5, 17, 25]
+
+    def __init__(self, pad='', in_blocks=False):
+        """Initialize an instance of Affine cipher with needed attributes"""
+        super().__init__(pad, in_blocks)
+        self.alpha, self.alpha_inv = self.get_alpha()
+        self.beta = self.get_beta()
+
+    def encrypt(self, text):
+        """
+        Takes a single argument (a text) and encrypts it using the
+        Affine encryption method.
+        """
+        positions = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.LETTERS.index(char)
+                positions.append(index)
+            except ValueError:
+                positions.append(char)
+        for index, num in enumerate(positions):
+            if isinstance(positions[index], int):
+                positions[index] = (num*self.alpha + self.beta) % 26
+
+        output = []
+        for num in positions:
+            if isinstance(num, int):
+                output.append(self.LETTERS[num])
+            else:
+                output.append(num)
+        return ''.join(output)
+
+    def decrypt(self, text):
+        """
+        Takes a single argument (an affine codified text) and decrypts it
+        using the Affine decryption method.
+        """
+        positions = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.LETTERS.index(char)
+                positions.append(index)
+            except ValueError:
+                positions.append(char)
+        for index, num in enumerate(positions):
+            if isinstance(positions[index], int):
+                positions[index] = (self.alpha_inv * (num - self.beta)) % 26
+
+        output = []
+        for num in positions:
+            if isinstance(num, int):
+                output.append(self.LETTERS[num])
+            else:
+                output.append(num)
+        return ''.join(output)
+
+    def get_alpha(self):
+        while True:
+            key = input("Choose a first key from this list: {}\n"
+                        "Or leave blank for default(5)> ".format(self.ALPHA))
+            if key == '':
+                return 5, 21
+            try:
+                alpha = int(key)
+            except ValueError:
+                print("Sorry, you must choose a valid key.\n")
+            else:
+                if alpha in self.ALPHA:
+                    index = self.ALPHA.index(alpha)
+                    alpha_inv = self.ALPHA_INV[index]
+                    return alpha, alpha_inv
+                print("Sorry, you must choose a valid key.\n")
+
+    def get_beta(self):
+        while True:
+            key = input("Choose a second positive numeric key. "
+                        "Leave blank for default (8)> ")
+            if key == '':
+                return 8
+            try:
+                beta = int(key)
+            except ValueError:
+                print("Sorry, key must be a positive integer\n")
+            else:
+                if beta > 0:
+                    return beta
+                print("Sorry, key must be a positive integer\n")
+
+
+class Atbash(Cipher):
+    """
+    Cipher that uses the Atbash encryption method to codify a message
+    """
+
+    def __init__(self, pad='', in_blocks=False):
+        """Initialize an instance of Atbash cipher with needed attributes"""
+        super().__init__(pad, in_blocks)
+        self.ORDERED = self.LETTERS
+        self.REVERSED = self.ORDERED[::-1]
+
+    def encrypt(self, text):
+        """
+        Takes a single argument (a text) and encrypts it using the
+        Atbash encryption method.
+        """
+        output = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.ORDERED.index(char)
+            except ValueError:
+                output.append(char)
+            else:
+                output.append(self.REVERSED[index])
+        return ''.join(output)
+
+    def decrypt(self, text):
+        """
+        Takes a single argument (an atbash codified text) and decrypts it
+        using the Atbash decryption method.
+        """
+        return self.encrypt(text)
+
+
+class Caesar(Cipher):
+    """
+    Cipher that uses the Caesar encryption method to codify a message
+    """
+
+    def __init__(self, pad='', in_blocks=False, offset=3):
+        """Initialize an instance of Caesar cipher with needed attributes"""
+        super().__init__(pad, in_blocks)
+        self.offset = offset
+        self.FORWARD = self.LETTERS + self.LETTERS[:self.offset+1]
+        self.BACKWARD = self.LETTERS[:self.offset+1] + self.LETTERS
+
+    def encrypt(self, text):
+        """
+        Takes a single argument (a text) and encrypt it using the
+        Caesar encryption method.
+        """
+        output = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.FORWARD.index(char)
+            except ValueError:
+                output.append(char)
+            else:
+                output.append(self.FORWARD[index+self.offset])
+        return ''.join(output)
+
+    def decrypt(self, text):
+        """
+        Takes a single argument (a caesar codified text) and decrypts it
+        using the Caesar decryption method.
+        """
+        output = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.BACKWARD.index(char)
+            except ValueError:
+                output.append(char)
+            else:
+                output.append(self.BACKWARD[index-self.offset])
+        return ''.join(output)
+
+
+class Keyword(Cipher):
+    """Cipher that uses the Keyword encryption method to codify a message
+    This method will require one input from user, a keyword, to code message.
+    """
+
+    def __init__(self, pad='', in_blocks=False):
+        """Initialize an instance of Keyword cipher with needed attributes"""
+        super().__init__(pad, in_blocks)
+        self.ORDERED = string.ascii_uppercase
+        self.ENCRYPTED = self.get_encrypted_dict()
+
+    def encrypt(self, text):
+        """
+        Takes a single argument (a text) and encrypts it using the
+        Keyword encryption method.
+        """
+        output = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.ORDERED.index(char)
+            except ValueError:
+                output.append(char)
+            else:
+                output.append(self.ENCRYPTED[index])
+        return ''.join(output)
+
+    def decrypt(self, text):
+        """
+        Takes a single argument (a keyword codified text) and decrypts it
+        using the Keyword decryption method.
+        """
+        output = []
+        text = text.upper()
+        for char in text:
+            try:
+                index = self.ENCRYPTED.index(char)
+            except ValueError:
+                output.append(char)
+            else:
+                output.append(self.ORDERED[index])
+        return ''.join(output)
+
+    def get_keyword(self):
+        """
+        Gets an alphanumeric keyword from user.
+        If not provided, it uses 'kryptos' as default keyword,
+        """
+        key = input(
+            "Use you're own keyword or leave blank for default (krypthos): "
+        ).upper()
+
+        if key == '':
+            return 'KRYPTHOS'
+        return self.clean_keyword(key)
+
+    def clean_keyword(self, keyword):
+        """
+        Removes any character repeated and spaces from the key provided.
+        """
+        key = []
+        for letter in keyword:
+            if letter not in key and letter != ' ':
+                key.append(letter)
+        return ''.join(key)
+
+    def get_encrypted_dict(self):
+        """
+        Creates an encrypted dictionary with the keyword provided by the
+        user, following the rules of the Keyword Cipher method.
+        """
+        encrypted_dict = self.get_keyword()
+
+        for letter in self.ORDERED:
+            if letter not in encrypted_dict:
+                encrypted_dict += letter
+        return encrypted_dict
--- secret_messages.py
+++ secret_messages.py
@@ -1,7 +1,3 @@
-import affine_cipher
-import atbash_cipher
-import caesar_cipher
-import keyword_cipher
 import os
 
 
@@ -13,176 +9,116 @@ def clear_screen():
 def show_menu():
     """Shows the main menu of Secret Messages program."""
     clear_screen()
-    print("""
-
-This is the Secret Messages project for the Treehouse Techdegree.
-
-These are the current availables ciphers:
-
--Caesar
--Atbash
--Affine
--Keyword
-
-Enter QUIT to exit Secret Messages.
-
-""")
+    print(
+        "This is the Secret Messages project for the Treehouse Techdegree.\n\n"
+        "These are the current availables ciphers:\n\n"
+        "-Caesar\n"
+        "-Atbash\n"
+        "-Affine\n"
+        "-Keyword\n\n"
+        "Enter QUIT to exit Secret Messages.\n\n"
+    )
 
 
-def get_cipher():
-    """Prompt a message to the user to choose one of the available ciphers."""
-    available_ciphers = ['CAESAR', 'ATBASH', 'AFFINE', 'KEYWORD']
+def get_cipher_or_quit():
+    """Prompts a message to the user to choose one of the available ciphers."""
+    available_ciphers = ['Caesar', 'Atbash', 'Affine', 'Keyword']
+    module = __import__('ciphers')
 
     while True:
-        cipher = input("Which cipher would you like to use? ").upper()
-        if cipher == 'QUIT':
+        cipher = input(
+            "Which cipher would you like to use? ").lower().capitalize()
+        if cipher == 'Quit':
             return False
-
         if cipher in available_ciphers:
-            if cipher == 'CAESAR':
-                return caesar_cipher.Caesar()
-            if cipher == 'ATBASH':
-                return atbash_cipher.Atbash()
-            if cipher == 'KEYWORD':
-                return keyword_cipher.Keyword()
-            if cipher == 'AFFINE':
-                return affine_cipher.Affine()
-            break
-        else:
-            print("Sorry, that's not a valid cipher.\n")
+            pad = get_pad()
+            blocks = in_blocks()
+            return getattr(module, cipher)(pad, blocks)
+        print("Sorry, that's not a valid cipher.\n")
 
 
 def get_message():
     """Prompts a message on screen to request a message from user."""
-    message = input("\nThat's an excellent cipher. What's the message? ")
+    message = input("What's the message? ")
     return message
 
 
 def get_option():
-    """Asks the user if wants to encrypt or decrypt the message."""
+    """
+    Asks the user if wants to encrypt or decrypt the message and returns
+    the option as a string to call the proper method of the cipher instance
+    """
     while True:
         try:
-            options = ["encrypt", "e", "decrypt", "d"]
-            option = input("\nAre we going to [e]ncrypt or [d]ecrypt? ")
-            if option in options:
+            option = input("Are we going to encode or decode? ")
+            if option == 'encode' or option == 'decode':
                 return option
             else:
                 raise ValueError
         except ValueError:
-            print("Sorry, you have to enter a valid command.")
-        else:
-            break
+            print("Sorry, you have to enter a valid command.\n")
 
 
 def get_pad():
-    """Let the user to use a new level of encryption with a one time pad key.
+    """
+    Lets the user to use a new level of encryption with a one time pad key.
     This key must be an integer.
     """
     while True:
-        pad = (input("\nEnter a pad number (leave blank for no pad): "))
+        pad = (input("Enter a pad number (leave blank for no pad): "))
         if pad == '':
             return pad
         else:
             try:
                 return str(int(pad))
             except ValueError:
-                print("\nSorry, you have to enter a valid numeric key pad")
-            else:
-                return pad
+                print("Sorry, you have to enter a valid numeric key pad\n")
 
 
 def in_blocks():
-    """Let the user to encrypt the message in blocks of five characters.
+    """
+    Let the user to encrypt the message in blocks of five characters.
     By default is set to NO.
     """
-    option = input("\nEncryption in blocks of 5? y/N> ").lower()
+    option = input("Encryption in blocks of 5? y/N> ").lower()
     if option == 'y':
         return True
-    else:
-        return False
+    return False
 
 
-def encrypt_message(cipher, message):
-    """Gets the message written by the user and all the options selected,
-    aswell the cipher chosen, and prints the encrypted message on screen.
+def show_message(cipher, option, message):
     """
-    pad = get_pad()
-
-    if pad == '':
-        encrypted_message = cipher.encrypt(message)
-        if in_blocks():
-            print(cipher.split_in_blocks(encrypted_message))
-        else:
-            print(encrypted_message)
-    else:
-        encrypted_message = cipher.encrypt(message)
-        pad_encrypted_message = cipher.pad_encryption(pad, encrypted_message)
-        if in_blocks():
-            print(cipher.split_in_blocks(pad_encrypted_message))
-        else:
-            print(pad_encrypted_message)
-
-
-def decrypt_message(cipher, message):
-    """Gets the encrypted message written by the user and all the options
-    selected, aswell the cipher chosen, and prints the decrypted message
-    on screen.
+    Uses the option to call the proper method on the cipher instance and
+    prints the encoded/decoded message on screen
     """
-    pad = get_pad()
-    if pad == '':
-        if in_blocks():
-            joined_message = cipher.join_from_blocks(message)
-            decrypted_message = cipher.decrypt(joined_message)
-            print(decrypted_message)
-        else:
-            decrypted_message = cipher.decrypt(message)
-            print(decrypted_message)
-    else:
-        if in_blocks():
-            message = cipher.join_from_blocks(message)
-            decrypted_message = cipher.pad_encryption(pad, message, False)
-            pad_decrypted_message = cipher.decrypt(decrypted_message)
-            print(pad_decrypted_message)
-        else:
-            decrypted_message = cipher.pad_encryption(pad, message, False)
-            pad_decrypted_message = cipher.decrypt(decrypted_message)
-            print(pad_decrypted_message)
-
-
-def process_message(cipher, option, message):
-    """Let the user select to encrypt or decrypt the message, once the option
-    is valid, the message is processed. Takes three arguments:
-    """
-    if option.lower() == 'encrypt' or option.lower() == 'e':
-        encrypt_message(cipher, message)
-
-    if option.lower() == 'decrypt' or option.lower() == 'd':
-        decrypt_message(cipher, message)
+    result = getattr(cipher, option)(message)
+    print("\nYour {}d message is: {}".format(option, result))
 
 
 def cipher_again():
-    """Ask the user if wants to encrypt or decrypt another message or exit
+    """
+    Ask the user if wants to encrypt or decrypt another message or exit
     the program. By default the answer is NO.
     """
     start = input("\nDo you want to encrypt or decrypt another message [Y/n]")
     if start.lower() != 'n':
-        main()
+        run()
     else:
-        print("\nThanks for using this Secret Messages project. Bye bye!")
+        print("Thanks for using this Secret Messages project. Bye bye!")
 
 
-def main():
+def run():
     """Main function of Secret Messages. Runs the main program."""
     show_menu()
-    cipher = get_cipher()
+    cipher = get_cipher_or_quit()
     if cipher:
         message = get_message()
         option = get_option()
-        process_message(cipher, option, message)
+        show_message(cipher, option, message)
         cipher_again()
     else:
-        print("\nThanks for using this Secret Messages project. Bye bye!")
+        print("Thanks for using this Secret Messages project. Bye bye!")
 
 
 if __name__ == '__main__':
-    main()
+    run()
